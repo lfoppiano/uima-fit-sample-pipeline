148ef74601d28d2c3781786160121c94dde487dd
lfoppiano <luca@foppiano.org>
2014-02-04 15:41:12 +0100

    added sofa mapping in the sample application

--- src/main/java/org/foppiano/uima/fit/tutorial/App.java
+++ src/main/java/org/foppiano/uima/fit/tutorial/App.java
@@ -2,11 +2,14 @@ package org.foppiano.uima.fit.tutorial;
 
 import org.apache.uima.UIMAException;
 import org.apache.uima.analysis_engine.AnalysisEngineDescription;
+import org.apache.uima.analysis_engine.metadata.SofaMapping;
 import org.apache.uima.annotator.WhitespaceTokenizer;
 import org.apache.uima.annotator.dict_annot.impl.DictionaryAnnotator;
 import org.apache.uima.collection.CollectionReaderDescription;
+import org.apache.uima.fit.factory.AggregateBuilder;
 import org.apache.uima.fit.factory.AnalysisEngineFactory;
 import org.apache.uima.fit.factory.CollectionReaderFactory;
+import org.apache.uima.fit.factory.SofaMappingFactory;
 import org.apache.uima.fit.pipeline.SimplePipeline;
 import org.foppiano.uima.fit.tutorial.annotator.SimpleParserAE;
 import org.foppiano.uima.fit.tutorial.casConsumer.SimpleCC;
@@ -31,6 +34,7 @@ public class App {
                 "/home/lf84914/development/workspace/uimaFitTestTutorial/src/main/resources/sampleInput.csv");
 
         AnalysisEngineDescription preparationEngine = AnalysisEngineFactory.createEngineDescription(SimpleParserAE.class);
+
         AnalysisEngineDescription dictionaryEngine = AnalysisEngineFactory.createEngineDescription(
                 DictionaryAnnotator.class,
                 "DictionaryFiles",
@@ -38,11 +42,17 @@ public class App {
                         "dictionary.xml"
                 },
                 "InputMatchType",
-                "org.apache.uima.TokenAnnotation",
-                "SofaNames",
-                new String[]{SimpleParserAE.SOFA_NAME_TEXT_ONLY}
-
+                "org.apache.uima.TokenAnnotation"
         );
+
+        /*try {
+            dictionaryEngine.toXML(new FileWriter(new File("/tmp/bao")));
+        } catch (SAXException e) {
+
+        }*/
+
+/*        SofaMapping mapper = SofaMappingFactory.createSofaMapping(dictionaryEngine.getClass().getCanonicalName(), "textOnly", "dictionary");
+        dictionaryEngine.setSofaMappings(new SofaMapping[]{mapper});*/
 
         AnalysisEngineDescription whitespaceEngine = AnalysisEngineFactory.createEngineDescription(WhitespaceTokenizer.class,
                 "SofaNames",
@@ -55,7 +65,14 @@ public class App {
                 "/home/lf84914/development/epo/apl/data/out"
         );
 
+        AggregateBuilder builder = new AggregateBuilder();
+        builder.add(preparationEngine);
+        builder.add(whitespaceEngine);
+        builder.add(dictionaryEngine, SimpleParserAE.SOFA_NAME_TEXT_ONLY, SimpleParserAE.SOFA_NAME_TEXT_ONLY);
+        //builder.addSofaMapping(DictionaryAnnotator.class.getName(), "textOnly", "dictionary");
 
-        SimplePipeline.runPipeline(reader, preparationEngine, whitespaceEngine, dictionaryEngine, casConsumer);
+
+  //      SimplePipeline.runPipeline(reader, preparationEngine, whitespaceEngine, dictionaryEngine, casConsumer);
+        SimplePipeline.runPipeline(reader, builder.createAggregateDescription(), casConsumer);
     }
 }
